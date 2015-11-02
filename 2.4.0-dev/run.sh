6544ee6a5174a96c5def4c4f591b17f023cad546
Marcelo Ansante <mansante@gmail.com>
2015-11-02 19:22:53 -0200

    fixed netty ip server and removed jmx

--- 2.4.0-dev/run.sh
+++ 2.4.0-dev/run.sh
@@ -16,17 +16,22 @@ if [ ! -d $CONFIG_DIR ]; then
     exit 1
 fi
 
+HORNETQ_HOST=`ip addr show eth0 | grep -oE '((1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])\.){3}(1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])'`;
+if [ a"$HORNETQ_HOST" = a ]; then HORNETQ_HOST=localhost; fi
+
 RESOLVED_CONFIG_DIR=`cd "$CONFIG_DIR"; pwd`
 export CLASSPATH=$RESOLVED_CONFIG_DIR:$HORNETQ_HOME/schemas/
 
 # Use the following line to run with different ports
-#export CLUSTER_PROPS="-Djnp.port=1099 -Djnp.rmiPort=1098 -Djnp.host=localhost -Dhornetq.remoting.netty.host=localhost -Dhornetq.remoting.netty.port=5445"
+#export CLUSTER_PROPS="-Djnp.port=1099 -Djnp.rmiPort=1098 -Djnp.host=$HORNETQ_HOST -Dhornetq.remoting.netty.host=$HORNETQ_HOST -Dhornetq.remoting.netty.port=5445"
 
 #export JVM_ARGS="$CLUSTER_PROPS -XX:+UseParallelGC -XX:+AggressiveOpts -XX:+UseFastAccessorMethods -Xms512M -Xmx1024M -Dhornetq.config.dir=$RESOLVED_CONFIG_DIR -Djava.util.logging.manager=org.jboss.logmanager.LogManager -Dlogging.configuration=file://$RESOLVED_CONFIG_DIR/logging.properties -Djava.library.path=./lib/linux-i686:./lib/linux-x86_64"
-#export JVM_ARGS="-Xmx512M -Djava.util.logging.manager=org.jboss.logmanager.LogManager -Dlogging.configuration=$CONFIG_DIR/logging.properties -Dhornetq.config.dir=$CONFIG_DIR -Djava.library.path=. -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=5005"
+
+# Use the following line to debug
+#export JVM_ARGS="-Djnp.host=$HORNETQ_HOST -Djava.rmi.server.hostname=$HORNETQ_HOST -Dhornetq.remoting.netty.host=$HORNETQ_HOST -Xmx512M -Djava.util.logging.manager=org.jboss.logmanager.LogManager -Dlogging.configuration=$CONFIG_DIR/logging.properties -Dhornetq.config.dir=$CONFIG_DIR -Djava.library.path=. -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=5005"
 
 # Use the following line to development mode
-export JVM_ARGS="-Xmx512M -Djava.util.logging.manager=org.jboss.logmanager.LogManager -Dlogging.configuration=$CONFIG_DIR/logging.properties -Dhornetq.config.dir=$CONFIG_DIR -Djava.library.path=. -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=6000 -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false"
+export JVM_ARGS="-Xmx512M -Djnp.host=$HORNETQ_HOST -Djava.rmi.server.hostname=$HORNETQ_HOST -Dhornetq.remoting.netty.host=$HORNETQ_HOST -Djava.util.logging.manager=org.jboss.logmanager.LogManager -Dlogging.configuration=$CONFIG_DIR/logging.properties -Dhornetq.config.dir=$CONFIG_DIR -Djava.library.path=. "
 
 for i in `ls $HORNETQ_HOME/lib/*.jar`; do
 	CLASSPATH=$i:$CLASSPATH
